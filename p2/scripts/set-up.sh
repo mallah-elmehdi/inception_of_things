9e991c94f95ac4196e272ac99dd1843abd6fb9d1
mallah-elmehdi <elmehdimallah@gmail.com>
2023-12-13 16:55:35 +0100

    updates --

--- p2/scripts/set-up.sh
+++ p2/scripts/set-up.sh
@@ -1,4 +1,3 @@
 sudo apt-get update 
-sudo apt-get install kubectl 
-curl -sfL https://get.k3s.io | sh - && cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && export KUBECONFIG=~/.kube/config
-kubectl apply -f $HOME/configs/app1-deployment.yaml -f $HOME/configs/app2-deployment.yaml -f $HOME/configs/app3-deployment.yaml -f $HOME/configs/ingress.yaml 
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --flannel-iface eth1 && chmod 777 /etc/rancher/k3s/k3s.yaml
+kubectl apply -f configs/app1-deployment.yaml -f configs/app2-deployment.yaml -f configs/app3-deployment.yaml -f configs/ingress.yaml 
