b4b5359fc63f037c86b43e6e4664c03f84858087
mallah-elmehdi <elmehdimallah@gmail.com>
2024-03-24 01:48:01 +0000

    update the permission

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -22,6 +22,7 @@ echo "------------------------------------- INSALLING"
 echo ""
 
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --flannel-iface eth1
+sudo chmod 777 /etc/rancher/k3s/k3s.yaml
 clear
 
 echo ""
--- p1/scripts/slave.sh
+++ p1/scripts/slave.sh
@@ -23,4 +23,5 @@ echo ""
 CLUSTER_TOKEN=$(sudo cat /vagrant/confs/node-token)
 echo $CLUSTER_TOKEN
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent" K3S_URL='https://192.168.56.110:6443' K3S_TOKEN=$CLUSTER_TOKEN sh -s - --flannel-iface eth1
+sudo chmod 777 /etc/rancher/k3s/k3s.yaml
 
